a44b949f4b72f92287aa7ba06de570ae317788f4
karu1325 <143956677+karu1325@users.noreply.github.com>
2024-03-19 15:41:17 +0000

    [Add]function

--- sample.js
+++ sample.js
@@ -1,4 +1,9 @@
-var nickname = "taro"
-console.log(nickname)
-nickname = "jiro"
-console.log(nickname)
+var str = "webcamp"
+
+function foo() {
+  console.log(str)
+  var str = "dmm webcamp"
+  console.log(str)
+}
+
+foo()
